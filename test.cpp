d3f4294496e24ac50953606fd3d784848a6dbfb3
TZYLT <TZY143@126.com>
2024-03-04 20:24:33 +0800

    20240304

--- test.cpp
+++ test.cpp
@@ -3,11 +3,9 @@
 #include <vector>
 #include <numeric>
 using namespace std;
-const long long mod=998244353;
-int a[100000000];
 int main(){
     for(int i=1;i<=1e8-10;i++)
         a[i]=i;
     long long ans=reduce(a+1,a+99999989,0,plus<long long>() );
     printf("%lld\n",ans);
-}大吉大利，今晚吃鸡 ---QianXiquq 20240304
+}大吉大利，今晚切题，兄弟姐妹come on我们一起。老OIer，老OIer他在这里。 ---QianXiquq 20240304
